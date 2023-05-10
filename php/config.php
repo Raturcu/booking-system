1fcc4978fa9caddc3a3778750adc878b9885d292
Raturcu <razvan.andrei.turcu97@gmail.com>
2023-05-10 09:58:04 +0300

    issue

--- php/config.php
+++ php/config.php
@@ -1,5 +1,5 @@
 <?php
-$con=mysqli_connect("129.151.227.25","raturcu","Mangalia123","ratysystem") or die("Couldn't connect!");
+$con=mysqli_connect("129.151.227.25","raturcu","Mangalia123!","ratsystem") or die("Couldn't connect!");
 
 
 ?>
