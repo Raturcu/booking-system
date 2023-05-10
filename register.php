04a717e569d1260d87874595ab7f00c1ced885c9
Raturcu <razvan.andrei.turcu97@gmail.com>
2023-05-10 09:55:52 +0300

    new settings

--- register.php
+++ register.php
@@ -11,26 +11,8 @@
             <div class="box form-box">
                 <?php
                 include("php/config.php");
-                if(isset($_POST['submit'])){
-                    $username=$_POST['username'];
-                    $email=$_POST['email'];
-                    $password_1=$_POST['password_1'];
-                    $password_2=$_POST['password_2'];
-
-                $verify_query=mysqli_query($con, "SELECT email from administrators WHERE email='$email'");
-                if(mysqli_num_rows($verify_query)!=0){
-                    echo "Error";
-                    
-
-                }
-                else{
-                    mysqli_query($con,"INSERT INTO administrators(username,email,password_1,password_2) VALUES('$username','$email','$password_1',$password_2)") or die("ERROR!");
-                    echo "Registration succesfully!";
-                        
-
-
-                }    
-                 ?>
+                include("php/register.php");
+                ?>
                 <header>Register</header>
                 <form action="" method="post">
                     <div class="field input">
@@ -58,7 +40,7 @@
                 </form>
                  
             </div>
-        <?php } ?> 
+            
         </div>
     </body>
 </html>
